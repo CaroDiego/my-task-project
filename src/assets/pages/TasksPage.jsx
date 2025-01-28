0122a0635596b804ea11e094b3e4ad4c12733bbb
CaroDiego <dicafre1404@gmail.com>
2025-01-28 13:43:07 +0100

    Creando y mostrando informacion

--- src/assets/pages/TasksPage.jsx
+++ src/assets/pages/TasksPage.jsx
@@ -1,10 +1,31 @@
+import { useContext } from "react";
 import HeaderComponent from "../components/HeaderComponent";
+import { TaskContext } from "../../context/task.context";
+import TaskCard from "../components/TaskCard";
+import CreateTask from "../components/CreateTask";
 
 function TasksPage() {
+  const { tasks } = useContext(TaskContext);
+
+  const taskCards = tasks.map((task) => (
+    <li key={tasks.id}>
+      <TaskCard task={task}></TaskCard>
+    </li>
+  ));
+
   return (
     <>
       <HeaderComponent></HeaderComponent>
-      <div>TasksPage</div>
+
+      <section id="tasks-page">
+        <h2 className="title">Tasks</h2>
+        <ul className="task-list">
+          <li>
+            <CreateTask></CreateTask>
+          </li>
+          {taskCards}
+        </ul>
+      </section>
     </>
   );
 }
--- src/context/task.context.jsx
+++ src/context/task.context.jsx
@@ -2,9 +2,43 @@ import { createContext, useState } from "react";
 const TaskContext = createContext();
 
 function TasksProviderWrapper(props) {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Comprar la cena",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Cocinar",
+      completed: true,
+    },
+    {
+      id: "3",
+      title: "Cenar",
+      completed: false,
+    },
+    {
+      id: "4",
+      title: "Lavar los platos",
+      completed: false,
+    },
+  ]);
+
+  const addTask = (newTask) => {
+    setTasks([newTask, ...tasks]);
+  };
+
+  const updateTask = (updatedTask) => {
+    const uptadedTasks = tasks.map((task) => {
+      if (task.id !== updatedTask.id) return task;
+      return updatedTask;
+    });
+
+    setTasks(uptadedTasks);
+  };
   return (
-    <TaskContext.Provider value={{ tasks, setTasks }}>
+    <TaskContext.Provider value={{ tasks, setTasks, addTask, updateTask }}>
       {props.children}
     </TaskContext.Provider>
   );
